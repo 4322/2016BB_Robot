985be7f1d748894503d8bc71d403e5551e823abe
Nicolas Machado <nxmq08@gmail.com>
2016-10-07 14:33:24 -0700

    Auto-commit from build at time: 10-07-2016 02:33:23

--- src/org/usfirst/frc/team4322/beachblitzrobot/commands/Group_Shoot.java
+++ src/org/usfirst/frc/team4322/beachblitzrobot/commands/Group_Shoot.java
@@ -6,8 +6,8 @@ public class Group_Shoot extends CommandGroup
 {
     public Group_Shoot()
     {
-        addSequential(new Shooter_RunFlywheels());
         addSequential(new Vision_AcquireGoal());
+        addSequential(new Shooter_RunFlywheels());
         addSequential(new Command_Delay(1.5));
         addSequential(new Shooter_StopFlywheels());
         }
