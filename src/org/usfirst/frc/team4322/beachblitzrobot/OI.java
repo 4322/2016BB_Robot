8e696be7566de5948db5e04d210e891af005ba3b
Nicolas Machado <nxmq08@gmail.com>
2016-10-07 14:25:37 -0700

    Auto-commit from build at time: 10-07-2016 02:25:35

--- src/org/usfirst/frc/team4322/beachblitzrobot/OI.java
+++ src/org/usfirst/frc/team4322/beachblitzrobot/OI.java
@@ -19,7 +19,8 @@ public class OI
         CoPilotController.getInstance().getShootButton().whileHeld(new Group_Shoot());
         CoPilotController.getInstance().getHoodDownButton().whenPressed(new Shooter_LowerHood());        
         CoPilotController.getInstance().getHoodUpButton().whenPressed(new Shooter_RaiseHood());
-        CoPilotController.getInstance().getFireButton().whileHeld(new Group_Fire());
+        CoPilotController.getInstance().getFireButton().whenPressed(new Feeder_LoadShooter());
+        CoPilotController.getInstance().getFireButton().whenReleased(new Feeder_StopFeeder());
         CoPilotController.getInstance().getStopShooter().whenPressed(new Shooter_StopFlywheels());
     }
 }
