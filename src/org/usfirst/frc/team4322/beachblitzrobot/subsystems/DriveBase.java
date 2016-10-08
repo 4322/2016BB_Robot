fd06ddcde39fcd742ada865ec8784d5a132c7f6e
Nicolas Machado <nxmq08@gmail.com>
2016-10-08 12:11:56 -0700

    Auto-commit from build at time: 10-08-2016 12:11:50

--- src/org/usfirst/frc/team4322/beachblitzrobot/subsystems/DriveBase.java
+++ src/org/usfirst/frc/team4322/beachblitzrobot/subsystems/DriveBase.java
@@ -56,7 +56,6 @@ public class DriveBase extends Subsystem
         talonFrontLeft.setFeedbackDevice(FeedbackDevice.QuadEncoder);
         talonFrontLeft.setEncPosition(0);
         talonFrontLeft.configEncoderCodesPerRev(RobotMap.ENCODER_TICKS_PER_REV);
-        robotDrive.setExpiration(.75);
         // Create instance of robotDrive
         robotDrive = new RobotDrive(talonFrontLeft,talonBackLeft,talonFrontRight,talonBackRight);
 
