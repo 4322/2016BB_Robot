ea04d6a36affb6e384346e52a62331d89db131cb
Nicolas Machado <nxmq08@gmail.com>
2016-10-07 18:39:27 -0700

    Auto-commit from build at time: 10-07-2016 06:39:25

--- src/org/usfirst/frc/team4322/beachblitzrobot/commands/AutoGroup_CrossCheval.java
+++ src/org/usfirst/frc/team4322/beachblitzrobot/commands/AutoGroup_CrossCheval.java
@@ -7,7 +7,8 @@ public class AutoGroup_CrossCheval extends CommandGroup
     public AutoGroup_CrossCheval()
     {
         addSequential(new Collector_Lower());
+        addSequential(new DriveBase_DriveDistance(6, .6));
+        addParallel(new Collector_Raise());
         addSequential(new DriveBase_DriveDistance(60, .8));
-        addSequential(new Collector_Raise());
     }
 }
