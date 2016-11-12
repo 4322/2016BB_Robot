4f98cdc00174da2070575f96bede67d1fba54a1a
Nicolas Machado <nxmq08@gmail.com>
2016-11-11 21:14:45 -0500

    Documentation

--- src/org/usfirst/frc/team4322/beachblitzrobot/vision/VisionThread.java
+++ src/org/usfirst/frc/team4322/beachblitzrobot/vision/VisionThread.java
@@ -36,13 +36,6 @@ public class VisionThread extends Thread
     public static int bMin = 0;
     @DashboardInputField(field = "Blue Max Value: ")
     public static int bMax = 255;
-    @DashboardInputField(field = "Camera Vertical FOV: ")
-    //logitech camera vFOV.
-    public static double vFOV = 2*Math.atan(Math.tan(Math.toRadians(78/2))*Math.sin(Math.atan(9./16.)));
-    @DashboardInputField(field = "Camera Horizontal FOV: ")
-    //logitech camera vFOV.
-    public static double hFOV = 51.f;
-
     Image frame = imaqCreateImage(ImageType.IMAGE_RGB, 0);
     Image binarizedFrame = imaqCreateImage(ImageType.IMAGE_U8, 0);
     Image display = imaqCreateImage(ImageType.IMAGE_RGB, 0);
@@ -79,32 +72,34 @@ public class VisionThread extends Thread
     {
         if (id == -1)
             id = IMAQdxOpenCamera("cam1",IMAQdxCameraControlMode.CameraControlModeController);
-        	criteria[0] = new ParticleFilterCriteria2(
-                MeasurementType.MT_AREA_BY_IMAGE_AREA, 0.25, 1.5, 0,
-                0);
-
+        	criteria[0] = new ParticleFilterCriteria2,MeasurementType.MT_AREA_BY_IMAGE_AREA, 0.25, 1.5, 0, 0);
         while (!abort)
         {
             try
             {
+                //Start Image Aquisition
             	IMAQdxStartAcquisition(id);
-                IMAQdxGetImage(id, frame,
-                        IMAQdxBufferNumberMode.BufferNumberModeBufferNumber, 0);
+                //Get Frame
+                IMAQdxGetImage(id, frame, IMAQdxBufferNumberMode.BufferNumberModeBufferNumber, 0);
+                //Stop Acquisiton
                 IMAQdxStopAcquisition(id);
+                //Get Imaze Size
                 GetImageSizeResult size = imaqGetImageSize(frame);
-                imaqColorThreshold(binarizedFrame, frame, 255, ColorMode.RGB,
-                        new Range(rMin, rMax), new Range(gMin, gMax),
-                        new Range(bMin, bMax));
+                //Color Threshold
+                imaqColorThreshold(binarizedFrame, frame, 255, ColorMode.RGB, new Range(rMin, rMax), new Range(gMin, gMax), new Range(bMin, bMax));
                 imaqDuplicate(display, binarizedFrame);
-                imaqParticleFilter4(binarizedFrame, binarizedFrame, criteria,
-                        filterOptions, null);
+                //Particle Filter
+                imaqParticleFilter4(binarizedFrame, binarizedFrame, criteria, filterOptions, null);
+                //Count particles
                 int numParticles = imaqCountParticles(binarizedFrame, 1);
                 RobotLogger.getInstance().log("Particle count: %d\n",numParticles);
                 ArrayList<VisionReport> objects = new ArrayList<>();
+                //for each particle
                 for (int i = 0; i < numParticles; i++)
                 {
+                    //measure shit
                     VisionReport vr = new VisionReport();
-                     
+
                     vr.area = imaqMeasureParticle(binarizedFrame, i, 0,
                             MeasurementType.MT_AREA);
                     vr.bboxwidth = imaqMeasureParticle(binarizedFrame, i, 0,
@@ -120,12 +115,9 @@ public class VisionThread extends Thread
                     vr.ypos = imaqMeasureParticle(binarizedFrame, i, 0,
                             MeasurementType.MT_CENTER_OF_MASS_Y);
                     vr.boundingBoxArea = vr.bboxheight * vr.bboxwidth;
-                    vr.areaScore = ratioToScore(
-                            (vr.area / (vr.boundingBoxArea)) * 3.0);
-
+                    vr.areaScore = ratioToScore((vr.area / (vr.boundingBoxArea)) * 3.0);
                     vr.aspect = ((double) vr.bboxwidth / vr.bboxheight);
-                    vr.aspectScore = ratioToScore(
-                            (vr.aspect / idealAspect) * (3.0 / 5.0));
+                    vr.aspectScore = ratioToScore((vr.aspect / idealAspect) * (3.0 / 5.0));
                     vr.score = (vr.aspectScore + vr.areaScore) / 2;
                     vr.relxpos = vr.xpos / size.width;
                     vr.relypos = vr.ypos / size.height;
@@ -139,56 +131,42 @@ public class VisionThread extends Thread
                     }
                     objects.add(vr);
                 }
+                //sort shit
                 objects.sort((x, y) -> x.score > y.score ? 1 : -1);
+                //log shit
                 for (VisionReport in : objects)
                 {
                     RobotLogger.getInstance().log("SCORE: %f\n", in.score);
-                    RobotLogger.getInstance().log(
-                            "AREA SCORE: %f ASPECT SCORE: %f\n", in.areaScore,
-                            in.aspectScore);
-                    RobotLogger.getInstance().log("AREA: %f ASPECT: %f\n",
-                            in.area, in.aspect);
-                    RobotLogger.getInstance().log("XPOS: %f YPOS: %f\n",
-                            in.xpos, in.ypos);
-                    RobotLogger.getInstance().log("XPOS-REL: %f YPOS-REL: %f\n",
-                            in.relxpos, in.relypos);
-//                    RobotLogger.getInstance().log("DISTANCE: %f\n", in.distance);
-//                    SmartDashboard.putNumber("TARGET DISTANCE (ft): ", in.distance);
-//                    SmartDashboard.putNumber("TARGET DISTANCE (in): ", in.distance*12);
-
+                    RobotLogger.getInstance().log("AREA SCORE: %f ASPECT SCORE: %f\n", in.areaScore, in.aspectScore);
+                    RobotLogger.getInstance().log("AREA: %f ASPECT: %f\n", in.area, in.aspect);
+                    RobotLogger.getInstance().log("XPOS: %f YPOS: %f\n", in.xpos, in.ypos);
+                    RobotLogger.getInstance().log("XPOS-REL: %f YPOS-REL: %f\n", in.relxpos, in.relypos);
                 }
+                //draw image
                 if (!(objects.size() == 0))
                 {
                     out = objects.get(0);
                     out.time = System.currentTimeMillis();
-                    Rect rec = new NIVision.Rect((int) out.bboxtop + 1,
-                            (int) out.bboxleft - 1, (int) out.bboxheight + 2,
-                            (int) out.bboxwidth + 2);
-                    imaqDrawShapeOnImage(display, display, rec,
-                            DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT,
-                            (float) 127.0);
-                    imaqDrawLineOnImage(display, display, DrawMode.DRAW_VALUE,
-                            new Point((int) out.xpos - 10, (int) out.ypos),
-                            new Point((int) out.xpos - 10, (int) out.ypos + 10),
-                            255f);
-                    imaqDrawLineOnImage(display, display, DrawMode.DRAW_VALUE,
-                            new Point((int) out.xpos, (int) out.ypos + 10),
-                            new Point((int) out.xpos, (int) out.ypos + 10),
-                            255f);
+                    Rect rec = new NIVision.Rect((int) out.bboxtop + 1, (int) out.bboxleft - 1, (int) out.bboxheight + 2, (int) out.bboxwidth + 2);
+                    imaqDrawShapeOnImage(display, display, rec, DrawMode.DRAW_VALUE, ShapeMode.SHAPE_RECT, (float) 127.0);
+                    imaqDrawLineOnImage(display, display, DrawMode.DRAW_VALUE, new Point((int) out.xpos - 10, (int) out.ypos), new Point((int) out.xpos - 10, (int) out.ypos + 10), 255f);
+                    imaqDrawLineOnImage(display, display, DrawMode.DRAW_VALUE, new Point((int) out.xpos, (int) out.ypos + 10), new Point((int) out.xpos, (int) out.ypos + 10), 255f);
                 } else
                 {
                     out = null;
                 }
+                //display image
                 CameraServer.getInstance().setImage(display);
-                RobotLogger.getInstance()
-                        .log("========END OF SCORES========\n");
+                RobotLogger.getInstance().log("========END OF SCORES========\n");
                 Thread.sleep(rateLimit);
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 RobotLogger.getInstance().exc("VisionThread.run()", ex); 
                 NIVision.IMAQdxCloseCamera(id);
                 Robot.vision.runThread();
-            } finally
+            }
+            finally
             {
             }
         }
